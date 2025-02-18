c0a8edb0ec4461196492a67727f189c4207e2e46
TianMy <tianmengyang@tidecloud.org>
2025-02-18 16:40:23 +0800

    打包同步更新版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
+import packageInfo from './package.json'
+import fs from 'fs';
+
+const outDir = 'dist';
+
+const version = packageInfo.version || '0.0.0';
+
+const fileName = `main.${version}`;
+
+
+function updateVersionInPackageInfo(packageInfo: Record<string, any>, version: string) {
+  packageInfo.version = version;
+  packageInfo.main = `./${outDir}/main.${version}.umd.cjs`;
+  packageInfo.module = `./${outDir}/main.${version}.js`;
+  packageInfo.exports = {
+    ...packageInfo.exports,
+    import: `./${outDir}/main.${version}.js`,
+    require: `./${outDir}/main.${version}.umd.cjs`
+  };
+  fs.writeFileSync('package.json', JSON.stringify(packageInfo, null, 2));
+
+}
+
+updateVersionInPackageInfo(packageInfo, version);
+
 
 export default defineConfig({
   build: {
+    outDir,
     lib: {
       entry: './lib/main.ts',
       name: 'api-proxy.js',
-      fileName: 'main',
+      fileName,
     },
     rollupOptions: {
       output: {
@@ -16,7 +42,7 @@ export default defineConfig({
           'body-parser': 'body-parser',
         }
       },
-      external: ['socket.io', 'socket.io-client', 'express','body-parser'],
+      external: ['socket.io', 'socket.io-client', 'express', 'body-parser'],
     },
   },
 });
